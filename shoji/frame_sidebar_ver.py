7a53081ccd271153d9281bd90be975217d1dcf3a
keisuke-shoji <6322039@ed.tus.ac.jp>
2024-11-01 16:05:54 +0900

    Add files via upload

---
+++ shoji/frame_sidebar_ver.py
@@ -0,0 +1,157 @@
+import flet as ft
+from flet import(
+    Text,
+    ElevatedButton,
+    PopupMenuItem,
+    Row,
+    IconButton,
+    AppBar,
+    Icon,
+    Container,
+    PopupMenuButton,
+    margin,
+    colors,
+    icons,
+    NavigationRail,
+    NavigationRailDestination,
+    NavigationRailLabelType,
+    Page,
+    alignment,
+    border_radius,
+    FloatingActionButton,
+    UserControl,
+    CrossAxisAlignment,
+)
+
+
+class AppHeader(ft.Column):
+    def __init__(self, file, edit, tool, display, page):
+        super().__init__()
+        self.page = page
+
+        next_button = ElevatedButton(text = "next")
+        back_button = ElevatedButton(text = "back")
+        undo_button = ElevatedButton(text = "undo")
+        display_button = ElevatedButton(text = "display")
+        self.appbar_items = [
+            PopupMenuItem(text = "settings"),
+            PopupMenuItem(),
+            PopupMenuItem(text = "help"),
+        ]
+
+        self.page.appbar = AppBar(
+            leading = Icon(icons.TRIP_ORIGIN_ROUNDED),
+            leading_width = 60,
+            title = Text(value = "Project name", size = 24, text_align = "center"),
+            center_title = False,
+            toolbar_height = 50,
+            bgcolor = colors.SURFACE_VARIANT,
+            actions = [
+                Container(
+                    content = Row(
+                        [
+                            display_button,
+                            undo_button,
+                            back_button,
+                            next_button,
+                            PopupMenuButton(
+                                items = self.appbar_items
+                            ),
+                        ],
+                        alignment = "spaceBetween",
+                    ),
+                    margin = margin.only(left = 25, right = 50)
+                )
+            ],
+        )
+
+    def build(self):
+        return self.page.appbar
+
+class Sidebar(UserControl):
+    def __init__(self):
+        super().__init__()
+        self.nav_rail_visible = True
+        self.nav_rail_items = [
+            NavigationRailDestination(
+                icon_content = Icon(icons.FOLDER_OUTLINED),
+                selected_icon_content = Icon(icons.FOLDER_OUTLINED),
+                label_content = Text("file"),
+            ),
+            NavigationRailDestination(
+                icon_content = Icon(icons.CREATE),
+                selected_icon_content = Icon(icons.CREATE_OUTLINED),
+                label_content = Text("edit"), 
+            ),
+            NavigationRailDestination(
+                icon_content = Icon(icons.SETTINGS),
+                selected_icon_content = Icon(icons.SETTINGS_OUTLINED),
+                label_content = Text("Settings"),
+            ),
+        ]
+
+        self.nav_rail = NavigationRail(
+            height = 300,
+            selected_index = None,
+            label_type=NavigationRailLabelType.ALL,
+            min_width = 100,
+            min_extended_width = 400,
+            group_alignment = -0.9,
+            destinations = self.nav_rail_items,
+            on_change=lambda e: print("Selected destination: ", e.control.selected_index),
+        )
+        self.toggle_nav_rail_button = IconButton(
+            icon = icons.ARROW_CIRCLE_LEFT,
+            icon_color = colors.BLUE_GREY_400,
+            selected = False,
+            selected_icon = icons.ARROW_CIRCLE_RIGHT,
+            on_click = self.toggle_nav_rail,
+            tooltip = "Collapse Nav Bar",   
+        )
+
+    def build(self):
+        self.view = Container(
+                content = Row(
+                [
+                    self.nav_rail,
+                    Container(
+                        bgcolor = colors.BLACK26,
+                        border_radius = border_radius.all(30),
+                        height = 220,
+                        alignment = alignment.center_right,
+                        width = 2
+                    ),
+                    self.toggle_nav_rail_button,
+                ],
+                expand = True,
+                vertical_alignment = CrossAxisAlignment.START,
+                visible = self.nav_rail_visible,
+            )
+        )
+        return self.view
+
+    def toggle_nav_rail(self, e):
+        self.nav_rail.visible = not self.nav_rail.visible
+        self.toggle_nav_rail_button.selected = not self.toggle_nav_rail_button.selected
+        self.toggle_nav_rail_button.tooltip = "Open Side Bar" if self.toggle_nav_rail_button.selected else "Collapse Side Bar"
+        self.view.update()
+        self.page.update()
+
+def main(page: ft.Page):
+    page.title = "Video Maker"
+    page.padding = 10
+    my_text = Text("Frame Preview Area")
+    AppHeader("file", "edit", "tool", "display", page)
+    sidebar = Sidebar()
+
+    layout = Row(
+        controls = [sidebar, my_text],
+        tight = False,
+        expand = True,
+        vertical_alignment = "start",
+            )
+
+    page.add(layout)
+    page.update()
+
+ft.app(target = main)
