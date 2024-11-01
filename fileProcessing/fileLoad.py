dfd9d174d5cdbd47bd99b1e55ca74473dd93cfd7
k3-splat <6322045@ed.tus.ac.jp>
2024-11-01 16:22:40 +0900

    ファイル関連の基本的な実装が完了

--- fileProcessing/fileLoad.py
+++ fileProcessing/fileLoad.py
@@ -12,19 +12,19 @@ from flet import (
 )
 
 class mkdir:
-    directory_name = []
+    directory_paths = []
 
-    def __init__(self, page):  # pageをコンストラクタに渡す
+    def __init__(self, page):
         self.page = page
-        self.selected_directory = ""
+        self.selected_directory = ''
 
     def get_directory_path(self, e: FilePickerResultEvent):
         self.selected_directory = e.path
         self.page.open(mkdir.mkdir_dlg)
     
     def make_directory(self, e):
-        self.selected_directory += mkdir.mkdir_dlg.content.value
-        mkdir.directory_name.append(self.selected_directory)
+        self.selected_directory = self.selected_directory + '/' + mkdir.mkdir_dlg.content.value
+        mkdir.directory_paths.append(self.selected_directory)
         os.mkdir(self.selected_directory)
         self.page.close(mkdir.mkdir_dlg)
 
@@ -33,11 +33,26 @@ class mkdir:
         modal=True,
         content=ft.TextField(),
         actions=[
-            ft.TextButton("作成", on_click=lambda e: None),
+            ft.TextButton("作成", on_click=lambda e: None), #ダミーだから，インスタンスを作ったときに実際の処理を書く
             ft.TextButton("キャンセル", on_click=lambda e: None)
         ]
     )
-    
+
+class input_material:
+    material_paths = []
+
+    def __init__(self, page):
+        self.page = page
+        self.path = Text()
+
+    def pick_files_result(self, e: ft.FilePickerResultEvent):
+        if e.files:
+            self.path.value = ", ".join(file.path for file in e.files)
+        else:
+            self.path.value = "Cancelled!"
+
+        input_material.material_paths.append(self.path.value)
+        self.path.update()
 
 def main(page: Page):
     make_directory = mkdir(page)
@@ -46,10 +61,13 @@ def main(page: Page):
     mkdir.mkdir_dlg.actions[0].on_click = make_directory.make_directory
     mkdir.mkdir_dlg.actions[1].on_click = lambda e: page.close(mkdir.mkdir_dlg)
 
-    page.title = "AlertDialog examples"
+    pick_material = input_material(page)
+    pick_material_dialog = FilePicker(on_result=pick_material.pick_files_result)
+
+    page.title = "ファイルのやり取り"
     page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
 
-    page.overlay.extend([get_directory_dialog])
+    page.overlay.extend([get_directory_dialog, pick_material_dialog])
 
     page.add(
         Row(
@@ -62,6 +80,19 @@ def main(page: Page):
                 ),
             ]
         ),
+        Row(
+            [
+                ft.ElevatedButton(
+                    "Pick files",
+                    icon=ft.icons.UPLOAD_FILE,
+                    on_click=lambda _: pick_material_dialog.pick_files(
+                        allow_multiple=True,
+                        allowed_extensions=["jpg", "png", "wav", "mp3"]
+                    ),
+                ),
+                pick_material.path,
+            ]
+        )
     )
 
 
