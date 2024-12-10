4eaa4a2ad5d0328c56f68297e80caf4e3732680c
k3-splat <6322045@ed.tus.ac.jp>
2024-12-10 14:44:33 +0900

    Revert "pickleで実装するためにcanvasの処理をmainViewに移動。Stackによるレイヤー方式を思いつく"
    This reverts commit 02b38c128e88757a067742d449451e4cad601a40.

--- Sunagawa/chooseProjectView.py
+++ Sunagawa/chooseProjectView.py
@@ -1,27 +1,17 @@
 import flet as ft
 from pathDatabase import pathDatabase
 from dialogs import NotFolderSelected
-from fileLoad import saveAndloadFile
 
 class projectList:
     projectName = "hoge"
-    projectObj = None
 
     @classmethod
     def setprojectName(cls, value):
         cls.projectName = value
 
     @classmethod
-    def setprojectObj(cls, value):
-        cls.projectObj = value
-
-    @classmethod
     def getprojectName(cls):
         return cls.projectName
-    
-    @classmethod
-    def getprojectObj(cls):
-        return cls.projectObj
 
     def __init__(self, page: ft.Page):
         self.page = page
@@ -55,13 +45,8 @@ class projectList:
     def openProject(self):
         for checkbox in self.checkboxes:
             if checkbox.value:
-                tag = checkbox.label
-                loading = saveAndloadFile()
-
-                projectList.setprojectName(tag)
-                projectList.setprojectObj(loading.loadfile(tag))
+                projectList.setprojectName(checkbox.label)
                 self.page.go("/mainView")
-
                 return
             
         banner_instance = NotFolderSelected(
